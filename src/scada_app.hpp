1f8998ba5757f1fb60569a9ebdd976530248b095
Kuznetsov Max <maakuznetsov@mai.education>
2025-12-09 00:56:46 +0300

    Добавлен класс Widget с комментами

--- src/scada_app.hpp
+++ src/scada_app.hpp
@@ -1,6 +1,9 @@
-#include<iostream>
-#include<string>
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <string>
 
+// Структуры данных из XML
 struct Button{
     std::string name;
     int y;
@@ -24,4 +27,38 @@ struct Output{
     int height;
     std::string value;
     std::string unit;
+};
+
+class Widget{
+    public:
+        Widget() = default;
+        virtual ~Widget() = default; // virtual - для правильного удаления наследников
+
+        // virtual - наследники могут переопределить метод, если =0 - наследники должны реализовать 
+        virtual void draw(sf::RenderTarget& target) const = 0; // Отрисовка объекта
+        virtual void update(float mouseX, float mouseY) = 0; // Обновление состояния (60 раз/сек)
+        virtual void handleEvent(const sf::Event& event, const sf::Vector2f& mousePos) = 0; // Обработка событий (клик, нажатие)
+        virtual bool contains(float x, float y) const = 0; // Проверка попадания мышки в ...
+        
+        // Наследники исп, как есть
+        void setPosition(float x, float y); // Координаты
+        void setSize(float w, float h); 
+        void setVariableName(const std::string& name); // name - имя виджета в XML
+        std::string getVariableName() const; // Возвращает имя
+
+    protected:
+        sf::Vector2f position; // Координаты виджета
+        sf::Vector2f size;
+        std::string variableName; // variableName - имя переменной 
+        bool visible = true; // Проверка состояния виджета (рисуется или нет)
+
+};
+class Button: public Widget{ 
+    
+};
+class TextField: public Widget{
+
+};
+class TextDisplay: public Widget{
+
 };
