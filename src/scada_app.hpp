9c9bb9bba3eb036dda22e9aad201c327bb6aea9f
Kuznetsov Max <maakuznetsov@mai.education>
2025-12-08 20:14:34 +0300

    Структуры переменных

---
+++ src/scada_app.hpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string>
+
+struct Button{
+    std::string name;
+    int y;
+    int x;
+    int width;
+    int height;
+    std::string text;
+};
+struct Input{
+    std::string name;
+    int y;
+    int x;
+    int width;
+    int height;
+};
+struct Output{
+    std::string name;
+    int y;
+    int x;
+    int width;
+    int height;
+    std::string value;
+    std::string unit;
+};
